efb14cfd32973e1c7c29763599b957bf6b652dd7
Shkumbim <mazrekajshkumbim@gmail.com>
2025-03-20 16:11:01 +0100

    Finished the project file
    updated everything i wanted in my query now i created the frontend file where i will run the streamlit part

--- projekti.py
+++ projekti.py
@@ -4,19 +4,20 @@ import pandas as pd
 # Create the engine
 engine = create_engine("postgresql://postgres:bbb1234556@localhost/Movies")
 
-def get_recommendations(genre):
+def get_recommendations(genre,start,end):
     query = """
     SELECT m.title, m.genres, AVG(r.rating) AS avg_rating
     FROM movies m
     JOIN ratings r ON m.movie_id = r.movie_id
-    WHERE m.genres LIKE %s AND m.num_rates >=50
+    WHERE m.genres LIKE %s AND m.num_rates >=50 
+    AND m.release_year>=%s AND m.release_year<=%s
     GROUP BY m.movie_id
     ORDER BY avg_rating DESC
     LIMIT 10;
     """
-    return pd.read_sql(query, engine, params=(f"%{genre}%",))
+    return pd.read_sql(query, engine, params=(f"%{genre }%",start,end))
 
 # Example usage
 genre = "Drama"
-df = get_recommendations(genre)
+df = get_recommendations(genre,1000,1990)
 print(df)
